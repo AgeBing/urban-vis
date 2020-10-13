4cef7fdf9ef9c47806e2830f2082aa4d3ba3cc56
ykjage <ykjage@gmail.com>
2020-10-13 18:03:58 +0800

    fix: 修改依赖

--- backend/app/service/taxi.js
+++ backend/app/service/taxi.js
@@ -1,33 +1,32 @@
 const { Service } = require('egg');
 const geoUtil = require('../util/geo')
 const timeUtil = require('../util/time')
-const heapdump = require('heapdump');
 
 // const TABLE = '20200618order_mini'
 const TABLE = 'taxigps20200618_filter_land_duplicate_slice'
 
-
-
 class TaxiService extends Service{
   static trajs
   /**
-   * 获取轨迹点
+   * 从数据库中获取轨迹点
    */
   async trajsPoints(){
-    console.log("查询 TABLE", TABLE)
-    const limit = 10000 * 20
+    const { ctx } = this
+    ctx.logger.info(`查询数据库表 ${TABLE} `)
+    const limit = 10000 * 200
     const offset = 0
     const sql = `SELECT LONGITUDE as lon, LATITUDE as lat, GPS_DATE as time, CARNO as carNo, SLICECOUNT as sliceCount FROM ${TABLE} LIMIT ${offset},${limit}`
     let points = await this.app.mysql.query(sql)
-    console.log('points length', points.length);
+    ctx.logger.info(`数据库获取 点数量  ${points.length}`);
     const used = process.memoryUsage().heapUsed / 1024 / 1024;
-    console.log(`Memory Used: ${Math.round(used)} MB`);
+    ctx.logger.info(`当前 Memory 用量 ${Math.round(used)} MB`)
     return points
   }
   /**
-   * 轨迹点变成轨迹
+   * 轨迹点变成轨迹 保存至静态变量
    */
   async trajectorys(){
+    const { ctx } = this
     if(!TaxiService.trajs){
       const points = await this.trajsPoints()
       TaxiService.trajs =  pointsToTrajs(points)
@@ -35,21 +34,20 @@ class TaxiService extends Service{
     return TaxiService.trajs
   }
   /**
-   * 按时间可空间查询
+   * 按时间和空间查询
    */
   async query(){
     const { ctx } = this
     const { time, region } = ctx.request.body
     let trajs = await this.trajectorys()
-    console.log("过滤前",trajs.length)
+    ctx.logger.info("过滤前 Taxi 数量", trajs.length)
     trajs = filterTrajInRegion(trajs, region, time)
-    console.log("过滤后",trajs.length)
+    ctx.logger.info("过滤后 Taxi 数量", trajs.length)
     const used = process.memoryUsage().heapUsed / 1024 / 1024;
-    console.log(`Memory Used: ${Math.round(used)} MB`);
+    ctx.logger.info(`当前 Memory 用量 ${Math.round(used)} MB`)
     return trajs
   }
 }
-
 
 /**
  * 轨迹点拼接成轨迹
@@ -59,7 +57,7 @@ const pointsToTrajs = (ps) => {
   let lastCarNo = ''
   let lastSliceCount = 0
   ps.forEach(point => {
-    const { lon, lat, time, carNo, sliceCount } = point
+    const { carNo, sliceCount } = point
     if(carNo != lastCarNo || sliceCount != lastSliceCount){
       trajs.push({
         carNo,
